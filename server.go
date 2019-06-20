cd8ac2804fb240d5db24fddfa485c1cc8b7d2b72
oneils <lex.nox@gmail.com>
2019-06-20 17:21:38 +0300

    Http Server:    - refactored ServeHTTP to isolate the different functionality into new functions.

--- server.go
+++ server.go
@@ -19,13 +19,27 @@ type PlayerServer struct {
 }
 
 func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	player := r.URL.Path[len("/players/"):]
-	score := p.store.GetPlayerScore(player)
+	switch r.Method {
+	case http.MethodPost:
+		p.processWin(w)
+	case http.MethodGet:
+		p.showScore(w, r)
+	}
+}
 
+func (p *PlayerServer) showScore(w http.ResponseWriter, r *http.Request) {
+	player := r.URL.Path[len("/players/"):]
+
+	score := p.store.GetPlayerScore(player)
 	if score == 0 {
 		w.WriteHeader(http.StatusNotFound)
 	}
+
 	fmt.Fprint(w, score)
+}
+
+func (p *PlayerServer) processWin(w http.ResponseWriter) {
+	w.WriteHeader(http.StatusAccepted)
 }
 
 type InMemoryPlayerStore struct{}
