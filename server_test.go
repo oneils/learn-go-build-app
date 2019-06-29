61c6c322b62ab9aa8e58f345564bc035d38dc7a3
oneils <lex.nox@gmail.com>
2019-06-29 18:42:18 +0300

    CommandLine and package structure:    - implemented base functionality for CLI

--- server_test.go
+++ server_test.go
@@ -94,13 +94,7 @@ func TestStoreWins(t *testing.T) {
 
 		assertStatus(t, response.Code, http.StatusAccepted)
 
-		if len(store.winCalls) != 1 {
-			t.Errorf("got %d calls to RecordWin want %d", len(store.winCalls), 1)
-		}
-
-		if store.winCalls[0] != player {
-			t.Errorf("did not store correct winner got '%s' want '%s'", store.winCalls[0], player)
-		}
+		assertPlayerWin(t, &store, "Pepper")
 	})
 }
 
@@ -182,5 +176,17 @@ func assertContentType(t *testing.T, response *httptest.ResponseRecorder, want s
 	t.Helper()
 	if response.Header().Get("content-type") != want {
 		t.Errorf("response did not have content-type of %s, got %v", want, response.HeaderMap)
+	}
+}
+
+func assertPlayerWin(t *testing.T, store *StubPlayerStore, winner string) {
+	t.Helper()
+
+	if len(store.winCalls) != 1 {
+		t.Fatalf("got %d calls to RecordWin want %d", len(store.winCalls), 1)
+	}
+
+	if store.winCalls[0] != winner {
+		t.Errorf("did not store correct winner got '%s' want '%s'", store.winCalls[0], winner)
 	}
 }
